0c68c0099f40c940bcc8047b899b1d802474b0fd
Aman Virmani <aman.virmani26@gmail.com>
2020-02-25 01:14:44 -0500

    completed AR Tag decoding

--- lena_superimpose.py
+++ lena_superimpose.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from collections import deque
 
 def four_point_transform(image, pts):
 	# obtain a consistent order of the points and unpack them
@@ -31,8 +32,13 @@ def four_point_transform(image, pts):
 	# compute the perspective transform matrix and then apply it
 	M = cv2.getPerspectiveTransform(rect, dst)
 	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	#lena = cv2.imread('./data/Lena.png')
+	#lena_resize = cv2.resize(lena, warped.shape)
+	#lena_warped = cv2.warpPerspective(lena_resize,np.linalg.inv(M),(maxWidth,maxHeight))
+	#cv2.imshow('test',lena_warped)
+	#cv2.waitKey(0)
 	# return the warped image
-	return warped
+	return warped, M
 
 def order_points(pts):
 	# initialzie a list of coordinates that will be ordered
@@ -54,47 +60,102 @@ def order_points(pts):
 	# return the ordered coordinates
 	return rect
 
-if __name__='__main__':
-    cap = cv2.VideoCapture('./data/Tag0.mp4')
-    # Check if camera opened successfully
-    if (cap.isOpened()== False):
-    	print("Error opening video stream or file")
-     
-    # Read until video is completed
-    while(cap.isOpened()):
-    	# Capture frame-by-frame
-    	ret, frame = cap.read()
-    	if ret == True:
-    		# Display the resulting frame
-    		cv2.imshow('Frame',frame)
-    		cv2.imwrite('test_frame.jpg',frame)
-    		gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    		gray = cv2.bilateralFilter(gray_frame, 15, 75, 75)
-    		ret, gray = cv2.threshold(gray_frame, 200, 255, 0)
-    		cnts, _ = cv2.findContours(gray.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    		cnts = sorted(cnts, key=cv2.contourArea,reverse=True)
-    		squares=[]
-    		for cnt in cnts:
-    			cnt_len = cv2.arcLength(cnt, True)
-    			cnt = cv2.approxPolyDP(cnt, 0.1*cnt_len, True)
-    			if len(cnt) == 4:
-    				if 2000 < cv2.contourArea(cnt) < 17500:
-    					squares.append(cnt)
-    		cv2.drawContours(frame, squares, -1, (255,128,0), 3)
-    					#cv2.drawContours(frame, cnt, -1, (255,128,0), 3)
-    
-    		cv2.imshow('Frame', frame)
-    		warped = four_point_transform(gray_frame, squares[0].reshape((4,2)))
-    		cv2.namedWindow('tag', cv2.WINDOW_KEEPRATIO)
-    		cv2.imshow('tag',warped)
-    		cv2.waitKey(0)
-    		break
-    		# Press Q on keyboard to  exit
-    		if cv2.waitKey(25) & 0xFF == ord('q'):
-    			break # Break the loop
-    	else:
-    		break
-    	# When everything done, release the video capture object
-    cap.release()
-    # Closes all the frames
-    cv2.DestroyAllWindows()
+def isWhite(tag):
+	ret, tag = cv2.threshold(tag,200,255,0)
+	count_white = cv2.countNonZero(tag)
+	#count_black_pixels = 0
+	#for i in range(tag.shape[0]):
+	#	for j in range(tag.shape[1]):
+	#		if tag[i][j] == 0 :
+	#			count_black_pixels += 1
+	return count_white > 0.9*tag.size
+
+def tagMatrix(tag):
+	tag = cv2.resize(tag,(200,200))
+	bit_size = int(tag.shape[0]/8)
+	a=0;b=0;
+	ar_tag = np.zeros((8,8))
+	for i in range(0,tag.shape[0],bit_size):
+		for j in range(0,tag.shape[1],bit_size):
+			if isWhite(tag[i:i+bit_size,j:j+bit_size]) :
+				ar_tag[a][b] = 1
+			else :
+				ar_tag[a][b] = 0
+			b +=1
+		a +=1
+		b = 0
+	return ar_tag
+
+def decode_tag(tag):
+	ar_tag = tagMatrix(tag)
+	tag_angle = orientation(ar_tag)
+	tag_id = deque([ar_tag[4,3],ar_tag[4,4],ar_tag[3,4],ar_tag[3,3]])
+	tag_id.rotate(tag_angle%90)
+	return tag_angle, tag_id
+
+
+def orientation(ar_tag):
+	if ar_tag[2, 2]:
+		orientation = 180
+	elif ar_tag[2, 5]:
+		orientation = 90
+	elif ar_tag[5, 2]:
+		orientation = 270
+	elif ar_tag[5, 5]:
+		orientation = 0
+	else :
+		orientation = None
+		print('No tag detected')
+	return orientation
+
+
+
+cap = cv2.VideoCapture('./data/Tag0.mp4')
+# Check if camera opened successfully
+if (cap.isOpened()== False):
+	print("Error opening video stream or file")
+ 
+# Read until video is completed
+while(cap.isOpened()):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	if ret == True:
+		# Display the resulting frame
+		cv2.imshow('Frame',frame)
+		cv2.imwrite('test_frame.jpg',frame)
+		gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray = cv2.bilateralFilter(gray_frame, 15, 75, 75)
+		ret, gray = cv2.threshold(gray_frame, 200, 255, 0)
+		cnts, _ = cv2.findContours(gray.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		cnts = sorted(cnts, key=cv2.contourArea,reverse=True)
+		squares=[]
+		for cnt in cnts:
+			cnt_len = cv2.arcLength(cnt, True)
+			cnt = cv2.approxPolyDP(cnt, 0.1*cnt_len, True)
+			if len(cnt) == 4:
+				if 2000 < cv2.contourArea(cnt) < 17500:
+					squares.append(cnt)
+		cv2.drawContours(gray, squares, -1, (255,128,0), 3)
+					#cv2.drawContours(frame, cnt, -1, (255,128,0), 3)
+
+		#cv2.imshow('Frame', gray)
+		warped, M = four_point_transform(gray_frame, squares[0].reshape((4,2)))
+		cv2.namedWindow('tag', cv2.WINDOW_KEEPRATIO)
+		cv2.imshow('tag', warped)
+		cv2.waitKey(0)
+		decode_tag(warped)
+		#break
+		lena = cv2.imread('./data/Lena.png')
+		lena_resize = cv2.resize(lena, warped.shape)
+		cv2.imshow('lena', lena_resize)
+		cv2.waitKey(0)
+		break
+		# Press Q on keyboard to  exit
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			break # Break the loop
+	else:
+		break
+	# When everything done, release the video capture object
+cap.release()
+# Closes all the frames
+#cv2.DestroyAllWindows()
