f2fdd35bef51885b377c7428e4c338eb3a3d211e
Aman Virmani <aman.virmani26@gmail.com>
2020-02-25 23:29:03 -0500

    Custom functions added

--- lena_superimpose.py
+++ lena_superimpose.py
@@ -27,40 +27,6 @@ def find_homography(source_pts, target_pts):
 
 	return H
 
-def four_point_transform(image, pts):
-	rect = order_points(pts)
-	(tl, tr, br, bl) = rect
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
-	# compute the perspective transform matrix and then apply it
-	M = cv2.getPerspectiveTransform(rect, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-	# return the warped image
-	return warped, M
-
-def order_points(pts):
-	rect = np.zeros((4, 2), dtype = "float32")
-	s = pts.sum(axis = 1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
-	# return the ordered coordinates
-	return rect
-
 def isWhite(tag):
 	ret, tag = cv2.threshold(tag,200,255,0)
 	count_white = cv2.countNonZero(tag)
@@ -88,7 +54,6 @@ def decode_tag(tag):
 	tag_id = deque([ar_tag[4,3],ar_tag[4,4],ar_tag[3,4],ar_tag[3,3]])
 	tag_id.rotate(tag_angle%90)
 	return tag_angle, tag_id
-
 
 def orientation(ar_tag):
 	if ar_tag[2, 2]:
@@ -119,12 +84,40 @@ def image_overlay(frame,pts,angle) :
 	#pts2 = np.float32(order_points(squares[0].reshape((4,2))))
 	pts2 = np.float32(order_points(pts))
 	M = cv2.getPerspectiveTransform(pts1,pts2)
-	#dst = warpPerspective(lena,M,(frame.shape[1],frame.shape[0]))
-	#cv2.imshow('dst',dst)
-	#cv2.waitKey(0)
-	dst = cv2.warpPerspective(lena,M,(frame.shape[1],frame.shape[0]))
+	dst = warpPerspective(lena,M,(frame.shape[1],frame.shape[0]))
+	cv2.imshow('dst', dst.astype(np.uint8))
+	cv2.waitKey(0)
+	#dst = cv2.warpPerspective(lena,M,(frame.shape[1],frame.shape[0]))
+	#dst = get_warp_perspective(lena,M,(frame.shape[1],frame.shape[0]))
 	overlay = cv2.add(frame,dst)
 	return overlay
+
+def to_img(mtr):
+	V,H,C = mtr.shape
+	img = np.zeros((H,V,C), dtype='int')
+	for i in range(mtr.shape[0]):
+		img[:,i] = mtr[i]
+	return img
+
+def to_mtx(img):
+	H,V,C = img.shape
+	mtr = np.zeros((V,H,C), dtype='int')
+	for i in range(img.shape[0]):
+		mtr[:,i] = img[i]
+	return mtr
+
+def warpPerspective(img, M, dsize):
+	mtr = to_mtx(img)
+	R,C = dsize
+	dst = np.zeros((R,C,mtr.shape[2]))
+	for i in range(mtr.shape[0]):
+		for j in range(mtr.shape[1]):
+			res = np.dot(M, [i,j,1])
+			i2,j2,_ = (res / res[2] + 0.5).astype(int)
+			if i2 >= 0 and i2 < R:
+				if j2 >= 0 and j2 < C:
+					dst[i2,j2] = mtr[i,j]
+	return to_img(dst)
 
 def superimpose():
 	frame = cv2.imread('test_frame.jpg')
@@ -148,8 +141,44 @@ def superimpose():
 		tgt_pts = order_points(np.array([[0,0],[0,std_size],[std_size,std_size],[std_size,0]]))
 		H = find_homography(src_pts, tgt_pts)
 		print(H)
+		lena = cv2.imread('data/Lena.png')
+		H_inv = np.linalg.inv(H)
+		H_inv /= H_inv[2,2]
+		dst = Warp(lena,H_inv,frame.shape)
+		#dst = cv2.warpPerspective(lena,H_inv,(frame.shape[1],frame.shape[0]))
+		#dst = get_warp_perspective(lena,H_inv,lena.shape)
+		#dst = wrap_final(lena,frame, H,(frame.shape[1],frame.shape[0]))
+		#dst = warpImage(lena,frame,H,(frame.shape[1],frame.shape[0]))
+		cv2.imshow('warp',dst)
+		cv2.waitKey(0)
+		print('finish')
 
+def Projection_matrix(H):
+# Calibration Matrix
+    K = np.array([[1406.08415449821,0,0],[2.20679787308599, 1417.99930662800,0],[1014.13643417416, 566.347754321696,1]])
+    K_reshape = np.reshape(K,(3,3))
+    K_trans = np.transpose(K_reshape)
+    K_inv = np.linalg.inv(K_trans)
+    K_inv_H = np.dot(K_inv,H)
+    K_inv_h1 = K_inv_H[:,0]
+    K_inv_h2 = K_inv_H[:,1]
+    lambd = 2/(np.linalg.norm(K_inv_h1)+np.linalg.norm(K_inv_h2))
 
+    if np.linalg.det(K_inv_H) < 0:
+        B = -lambd*K_inv_H
+    else:
+        B = lambd*K_inv_H
+
+    b1 = B[:,0]
+    b2 = B[:,1]
+    b3 = B[:,2]
+    r1 = b1
+    r2 = b2
+    r3 = np.cross(r1,r2)
+    t=b3
+    R_t=np.vstack([r1,r2,r3,t])
+    P = np.dot(K_trans,np.transpose(R_t))
+    return P
 
 def decodeTag():
 	cap = cv2.VideoCapture('./data/Tag0.mp4')
@@ -177,7 +206,7 @@ def decodeTag():
 
 			cv2.drawContours(frame, squares, -1, (255,128,0), 3)
 
-			warped,M = four_point_transform(gray, squares[0].reshape((4,2)))
+			#warped,M = four_point_transform(gray, squares[0].reshape((4,2)))
 
 			tag_angle, tag_id = decode_tag(warped)
 			final = image_overlay(frame, squares[0].reshape((4,2)),tag_angle)
@@ -190,5 +219,53 @@ def decodeTag():
 	cap.release()
 	cv2.DestroyAllWindows()
 
+def projectCube():
+	cap = cv2.VideoCapture('./data/Tag0.mp4')
+	# Check if camera opened successfully
+	if (cap.isOpened() == False):
+			print("Error opening video stream or file")
+	while (cap.isOpened()):
+		# Capture frame-by-frame
+		ret, frame = cap.read()
+		ret, frame = cap.read()
+		if ret == True:
+			if ret == True:
+				gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				gray = cv2.bilateralFilter(gray_frame, 15, 75, 75)
+				gray = cv2.bilateralFilter(gray_frame, 15, 75, 75)
+				ret, gray = cv2.threshold(gray_frame, 200, 255, 0)
+				ret, gray = cv2.threshold(gray_frame, 200, 255, 0)
+				cnts, _ = cv2.findContours(gray.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+				cnts, _ = cv2.findContours(gray.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+				cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+				cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+				squares = []
+				squares = []
+				for cnt in cnts:
+					for cnt in cnts:
+						cnt_len = cv2.arcLength(cnt, True)
+						cnt_len = cv2.arcLength(cnt, True)
+						cnt = cv2.approxPolyDP(cnt, 0.1 * cnt_len, True)
+						cnt = cv2.approxPolyDP(cnt, 0.1 * cnt_len, True)
+						if len(cnt) == 4:
+							if len(cnt) == 4:
+								if 2000 < cv2.contourArea(cnt) < 17500:
+									if 2000 < cv2.contourArea(cnt) < 17500:
+										squares.append(cnt)
+										squares.append(cnt)
+				cv2.drawContours(frame, squares, -1, (255, 128, 0), 3)
+				warped, M = four_point_transform(gray, squares[0].reshape((4, 2)))
+				tag_angle, tag_id = decode_tag(warped)
+				P = Projection_matrix(M)
+
 if __name__=='__main__':
-	superimpose()
+	#superimpose()
+	decodeTag()
+	#lena = cv2.imread('data/Lena.png')
+	#lena = cv2.resize(lena,(200,200))
+	#M = np.array([0.5531129032258064,0.08637096774193548,1053,-0.04280645161290323,0.25533870967741934,552,0.00018548387096774192,-0.000011290322580645161,1])
+	#M =M.reshape((3,3))
+	#dst = warpPerspective(lena,M,(512,512))
+	#cv2.imshow('dst',dst.astype(np.uint8))
+	#cv2.waitKey(0)
